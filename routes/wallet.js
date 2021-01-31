360041dbb72dbdad260bfb3e642d407ae67f92d6
Rewan Gameel <rewangameelrr2008@gmail.com>
2021-01-31 20:38:34 +0200

    reward points convertion

--- routes/wallet.js
+++ routes/wallet.js
@@ -5,5 +5,6 @@ const PurchaseController = require('../controllers/PurchaseController');
 
 // purchase routes
 router.put("/calculatePoints", PurchaseController.calculatePoints)
+router.put("/convertPoints", PurchaseController.convertPoints)
 
 module.exports = router;
